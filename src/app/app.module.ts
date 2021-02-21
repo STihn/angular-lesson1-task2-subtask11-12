ab1e63e8d52f7a603a7071a3f76a281af79274a4
STihn <you.lucky@bk.ru>
2021-02-22 01:06:23 +0300

    правки

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { Com1Component } from './com1/com1.component';
-import { Com2Component } from './com2/com2.component';
-import { Com3Component } from './com3/com3.component';
-import { Com4Component } from './com4/com4.component';
+import { Com1Component } from './component1/com1.component';
+import { Com2Component } from './component2/com2.component';
+import { Com3Component } from './component3/com3.component';
+import { Com4Component } from './component4/com4.component';
 import { Mod1Module } from './mod1/mod1.module';
 import { Mod2Module } from './mod2/mod2.module';
 
